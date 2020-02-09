f3d43559322b2f1276d11adae972c7a658b02a23
Pauloa90 <paulo.a90@hotmail.com>
2020-02-09 22:56:34 +0000

    git commit -m

--- ex019.py
+++ ex019.py
@@ -1,4 +1,11 @@
 import random
+import time
 alunos = ['Joao', 'Jessica', 'Paulo', 'Carol']
 n = random.randint(1, 4)
-print(n)
+print('Os alunos sao:')
+for x in alunos:
+    print(x)
+print('Entre esses o sorteado foi:')
+time.sleep(2)
+
+print(alunos[n])
